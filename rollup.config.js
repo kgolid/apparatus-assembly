547e7be41fcdeabc5c123564cdffbfe0e86f4c57
Kjetil Golid <kjetil.golid@gmail.com>
2018-12-02 02:47:33 +0100

    remove commented code

--- rollup.config.js
+++ rollup.config.js
@@ -28,17 +28,4 @@ export default [
       commonjs() // so Rollup can convert `ms` to an ES module
     ]
   }
-  /*
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/main.js',
-    external: ['ms'],
-    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }]
-  }
-  */
 ];
