4a78c2c840853237a43f83ff395416f08f00fa75
Kjetil Golid <kjetil.golid@gmail.com>
2018-12-02 02:48:50 +0100

    update distribution

--- dist/bundle-multi.js
+++ dist/bundle-multi.js
@@ -18,7 +18,8 @@
         solidness = 0.5,
         colors = [],
         color_mode = 'group',
-        group_size = 0.8
+        group_size = 0.8,
+        simple = false,
       } = {}
     ) {
       this.xdim = Math.round(width * 2 + 11, 0);
@@ -35,6 +36,7 @@
       this.v_symmetric = vertical_symmetry;
       this.roundness = roundness;
       this.solidness = solidness;
+      this.simple = simple;
     }
 
     generate() {
@@ -157,17 +159,19 @@
       // ---- Decisions ----
 
       function start_new_from_blank(x, y) {
+        if (context.simple) return true;
         if (!active_position(x, y, -1 * (1 - context.roundness))) return false;
         return Math.random() <= context.solidness;
       }
 
       function start_new(x, y) {
+        if (context.simple) return true;
         if (!active_position(x, y, 0)) return false;
         return Math.random() <= context.chance_new;
       }
 
       function extend(x, y) {
-        if (!active_position(x, y, 1 - context.roundness)) return false;
+        if (!active_position(x, y, 1 - context.roundness) && !context.simple) return false;
         return Math.random() <= context.chance_extend;
       }
 
@@ -243,27 +247,100 @@
     },
     {
       name: 'tundra3',
-      colors: ['#87c3ca', '#7b7377', '#b2475d', '#7d3e3e', '#eb7f64', '#d9c67a', '#f3f2f2']
+      colors: [
+        '#87c3ca',
+        '#7b7377',
+        '#b2475d',
+        '#7d3e3e',
+        '#eb7f64',
+        '#d9c67a',
+        '#f3f2f2'
+      ]
     },
     {
       name: 'tundra4',
-      colors: ['#d53939', '#b6754d', '#a88d5f', '#524643', '#3c5a53', '#7d8c7c', '#dad6cd']
+      colors: [
+        '#d53939',
+        '#b6754d',
+        '#a88d5f',
+        '#524643',
+        '#3c5a53',
+        '#7d8c7c',
+        '#dad6cd'
+      ]
     },
     {
       name: 'retro',
-      colors: ['#69766f', '#9ed6cb', '#f7e5cc', '#9d8f7f', '#936454', '#bf5c32', '#efad57']
+      colors: [
+        '#69766f',
+        '#9ed6cb',
+        '#f7e5cc',
+        '#9d8f7f',
+        '#936454',
+        '#bf5c32',
+        '#efad57'
+      ]
     },
     {
       name: 'retro-washedout',
-      colors: ['#878a87', '#cbdbc8', '#e8e0d4', '#b29e91', '#9f736c', '#b76254', '#dfa372']
+      colors: [
+        '#878a87',
+        '#cbdbc8',
+        '#e8e0d4',
+        '#b29e91',
+        '#9f736c',
+        '#b76254',
+        '#dfa372'
+      ]
     },
     {
       name: 'roygbiv-warm',
-      colors: ['#705f84', '#687d99', '#6c843e', '#fc9a1a', '#dc383a', '#aa3a33', '#9c4257']
+      colors: [
+        '#705f84',
+        '#687d99',
+        '#6c843e',
+        '#fc9a1a',
+        '#dc383a',
+        '#aa3a33',
+        '#9c4257'
+      ]
     },
     {
       name: 'roygbiv-toned',
-      colors: ['#817c77', '#396c68', '#89e3b7', '#f59647', '#d63644', '#893f49', '#4d3240']
+      colors: [
+        '#817c77',
+        '#396c68',
+        '#89e3b7',
+        '#f59647',
+        '#d63644',
+        '#893f49',
+        '#4d3240'
+      ]
+    },
+    {
+      name: 'present-correct',
+      colors: [
+        '#fd3741',
+        '#fe4f11',
+        '#ff6800',
+        '#ffa61a',
+        '#ffc219',
+        '#ffd114',
+        '#fcd82e',
+        '#f4d730',
+        '#ced562',
+        '#8ac38f',
+        '#79b7a0',
+        '#72b5b1',
+        '#5b9bae',
+        '#6ba1b7',
+        '#49619d',
+        '#604791',
+        '#721e7f',
+        '#9b2b77',
+        '#ab2562',
+        '#ca2847'
+      ]
     }
   ];
 
